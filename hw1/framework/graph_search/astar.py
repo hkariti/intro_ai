7c4b900c1d1e455fd9637533349351775a7f74ce
Hagai Kariti <hkariti@gmail.com>
2018-11-22 12:48:06 +0200

    ex 9

--- hw1/framework/graph_search/astar.py
+++ hw1/framework/graph_search/astar.py
@@ -41,13 +41,12 @@ class AStar(BestFirstSearch):
          whenever just after creating a new successor node.
         Should calculate and return the f-score of the given node.
         This score is used as a priority of this node in the open priority queue.
-
-        TODO: implement this method.
-        Remember: In Weighted-A* the f-score is defined by ((1-w) * cost) + (w * h(state)).
-        Notice: You may use `search_node.cost`, `self.heuristic_weight`, and `self.heuristic_function`.
         """
 
-        raise NotImplemented()  # TODO: remove!
+        w = self.heuristic_weight
+        h = self.heuristic_function.estimate
+        cost = search_node.cost
+        return (1-w) * cost + w * h(search_node.state)
 
     def _open_successor_node(self, problem: GraphProblem, successor_node: SearchNode):
         """
@@ -56,16 +55,19 @@ class AStar(BestFirstSearch):
         This method is responsible for adding this just-created successor
          node into the `self.open` priority queue, and may check the existence
          of another node representing the same state in `self.close`.
-
-        TODO: implement this method.
-        Have a look at the implementation of `BestFirstSearch` to have better understanding.
-        Use `self.open` (SearchNodesPriorityQueue) and `self.close` (SearchNodesCollection) data structures.
-        These data structures are implemented in `graph_search/best_first_search.py`.
-        Note: The successor_node's f-score has been already calculated and stored
-              under `successor_node.expanding_priority`.
-        Remember: In A*, in contrast to uniform-cost, a successor state might have an already closed node,
-                  but still could be improved.
         """
+        closed_node_with_same_state = self.close.get_node_by_state(successor_node.state)
+        if closed_node_with_same_state:
+            if closed_node_with_same_state.expanding_priority > successor_node.expanding_priority:
+                self.close.remove_node(closed_node_with_same_state)
+                self.open.push_node(successor_node)
+            return
 
-        raise NotImplemented()  # TODO: remove!
+        open_node_with_same_state = self.open.get_node_by_state(successor_node.state)
+        if open_node_with_same_state:
+            if open_node_with_same_state.expanding_priority > successor_node.expanding_priority:
+                self.open.extract_node(open_node_with_same_state)
+                self.open.push_node(successor_node)
+        else:
+            self.open.push_node(successor_node)
 
