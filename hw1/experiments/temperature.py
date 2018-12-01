f25f13db4d003c65463f94eafd182e82570c3532
Hagai Kariti <hkariti@gmail.com>
2018-12-01 17:35:01 +0200

    ex 20

--- hw1/experiments/temperature.py
+++ hw1/experiments/temperature.py
@@ -3,8 +3,18 @@ from matplotlib import pyplot as plt
 
 X = np.array([400, 450, 900, 390, 550])
 
-# TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+T = np.linspace(0.01, 5, 100)
+
+X_len = X.shape[0]
+T_len = T.shape[0]
+# Convert T to -1/T
+T_exp = -1/(T.reshape([1, T_len]))
+# Convert X to X**(-1/T)
+X_exp = np.power(X.reshape([X_len, 1]), T_exp)
+# Calcualte sum of all X**(-1/T)
+X_sum = X_exp.sum(axis=0)
+# Now we get P by definition. Transpose is to be compatible with the below for loop
+P = (X_exp / X_sum).transpose()
 
 print(P)
 
