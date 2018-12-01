a3206f82727525bd1e4f51ee262f59a30f11aa3a
Hagai Kariti <hkariti@gmail.com>
2018-12-01 20:53:42 +0200

    Use alpha here too

--- hw1/experiments/temperature.py
+++ hw1/experiments/temperature.py
@@ -9,8 +9,10 @@ X_len = X.shape[0]
 T_len = T.shape[0]
 # Convert T to -1/T
 T_exp = -1/(T.reshape([1, T_len]))
+# Get the normalizer constant
+alpha = np.min(X)
 # Convert X to X**(-1/T)
-X_exp = np.power(X.reshape([X_len, 1]), T_exp)
+X_exp = np.power(X.reshape([X_len, 1])/alpha, T_exp)
 # Calcualte sum of all X**(-1/T)
 X_sum = X_exp.sum(axis=0)
 # Now we get P by definition. Transpose is to be compatible with the below for loop
