88bd8c0fe9d37830b399851c818efa118c448e4c
Hagai Kariti <hkariti@gmail.com>
2019-01-15 15:45:17 +0200

    Add cross validation functions (part b q 3)

--- hw3/utils.py
+++ hw3/utils.py
@@ -1,6 +1,31 @@
+import pickle
 import numpy as np
 
 def euclidean_distance(x1, x2):
     diff_squared = (x1 - x2)**2
     distance_squared = diff_squared.sum(axis=0)
     return np.sqrt(distance_squared)
+
+def split_crosscheck_groups(dataset, num_folds):
+    samples, labels = dataset
+    labels = np.array(labels)
+    dataset_size = samples.shape[0]
+    indices = np.arange(dataset_size)
+    np.random.shuffle(indices)
+    fold_size = int(dataset_size/num_folds)
+    for i in range(num_folds):
+        start_fold = i * fold_size
+        end_fold = (i + 1) * fold_size
+        fold_indices = indices[start_fold:end_fold]
+        fold = (samples[fold_indices], labels[fold_indices])
+
+        filename = "ecg_fold_{}.data".format(i+1)
+        data_file = open(filename, 'wb')
+        pickle.dump(fold, data_file)
+        data_file.close()
+
+def load_k_fold_data(fold_id):
+    filename = "ecg_fold_{}.data".format(fold_id)
+    data_file = open(filename, 'rb')
+    fold = pickle.load(data_file)
+    return fold
