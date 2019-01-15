159a9bd02065ecd09f6e9aee57050b1e581af174
Hagai Kariti <hkariti@gmail.com>
2019-01-15 15:02:57 +0200

    Add knn implementation (part b q2)

---
+++ hw3/classifier.py
@@ -0,0 +1,26 @@
+import numpy as np
+from hw3_utils import abstract_classifier, abstract_classifier_factory
+from utils import euclidean_distance
+
+class knn_classifier(abstract_classifier):
+    def __init__(self, k, train_set, train_labels):
+        self._k = k
+        self._train_set = train_set
+        self._train_labels = train_labels
+
+    def classify(self, features):
+        num_samples = len(self._train_labels)
+        distances = np.ndarray(num_samples)
+        for i in range(num_samples):
+            distances[i] = euclidean_distance(features, self._train_set[i])
+        knn = np.argsort(distances)[:self._k]
+        knn_labels = self._train_labels[knn]
+        majority = np.round(knn_labels.mean())
+        return bool(majority)
+
+class knn_factory(abstract_classifier_factory):
+    def __init__(self, k):
+        self.k = k
+
+    def train(self, data, labels):
+        return knn_classifier(self.k, data, labels)
--- hw3/utils.py
+++ hw3/utils.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def euclidean_distance(x1, x2):
     diff_squared = (x1 - x2)**2
-    distance_squared = diff_squared.sum()
+    distance_squared = diff_squared.sum(axis=0)
     return np.sqrt(distance_squared)
