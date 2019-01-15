1ede22c295dbf51ba9d259a4229f9f29e44ebfa4
Hagai Kariti <hkariti@gmail.com>
2019-01-15 16:37:34 +0200

    Finsiha nd clean up validator

--- hw3/cross_validation.py
+++ hw3/cross_validation.py
@@ -25,10 +25,28 @@ def load_k_fold_data(fold_id):
     fold = pickle.load(data_file)
     return fold
 
+def pop_test_fold(samples, labels, test_fold):
+    num_folds = samples.shape[0]
+    train_folds = list(range(num_folds))
+    train_folds.pop(test_fold)
+    train_samples = np.concatenate(samples[train_folds])
+    train_labels = np.concatenate(labels[train_folds])
+
+    return train_samples, train_labels
+
+def test_classifier(classifier, test_samples, test_labels):
+    fold_size = len(test_labels)
+    output_labels = np.ndarray(fold_size, dtype='bool')
+    for sample_id in range(fold_size):
+        sample = test_samples[sample_id]
+        output_labels[sample_id] = classifier.classify(sample)
+    mean_error = np.abs(output_labels ^ test_labels).mean()
+
+    return mean_error
+
 def evaluate(classifier_factory, k):
     fold_samples = []
     fold_labels = []
-    fold_ids = list(range(k))
     for i in range(k):
         samples, labels = load_k_fold_data(i + 1)
         fold_samples.append(samples)
@@ -36,21 +54,19 @@ def evaluate(classifier_factory, k):
     # Convert to numpy arrays for easier indexing
     fold_samples = np.array(fold_samples)
     fold_labels = np.array(fold_labels)
-    fold_size = len(fold_labels[0])
 
     error_rates = []
+    accuracy_rates = []
     for test_fold in range(k):
-        train_folds = fold_ids[:]
-        train_folds.pop(test_fold)
-        train_samples = np.concatenate(fold_samples[train_folds])
-        train_labels = np.concatenate(fold_labels[train_folds])
+        train_samples, train_labels = pop_test_fold(fold_samples, fold_labels, test_fold)
         test_samples = fold_samples[test_fold]
         test_labels = fold_labels[test_fold]
 
         classifier = classifier_factory.train(train_samples, train_labels)
-        output_labels = np.array(fold_size, dtype='bool')
-        for sample_id in range(fold_size):
-            sample = test_samples[sample_id]
-            output_labels[sample_id] = classifier.classify(sample)
-        mean_error = np.abs(output_labels - test_labels).mean()
+        mean_error = test_classifier(classifier, test_samples, test_labels)
         error_rates.append(mean_error)
+        accuracy_rates.append(1 - mean_error)
+
+    accuracy = np.mean(accuracy_rates)
+    error = np.mean(error_rates)
+    return accuracy, error
