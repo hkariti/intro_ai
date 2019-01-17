f885e940412d9624000b09fc204f1a32c098e72a
Hagai Kariti <hkariti@gmail.com>
2019-01-17 12:47:40 +0200

    first version of contest classifier

--- hw3/classifier.py
+++ hw3/classifier.py
@@ -43,10 +43,38 @@ class sklearn_classifier(abstract_classifier):
 
 class tree_factory(abstract_classifier_factory):
     def train(self, data, labels):
-        t = tree.DecisionTreeClassifier()
+        t = tree.DecisionTreeClassifier(max_depth=3)
         return sklearn_classifier(t, data, labels)
 
 class perceptron_factory(abstract_classifier_factory):
     def train(self, data, labels):
         p = Perceptron(tol=1e-4)
         return sklearn_classifier(p, data, labels)
+
+class contest_classifier(abstract_classifier):
+    def __init__(self, tree_depth, full_weight, data, labels):
+        self.full_weight = full_weight
+        self.full_tree = tree.DecisionTreeClassifier(max_depth=tree_depth)
+        self.partial_tree = tree.DecisionTreeClassifier(max_depth=tree_depth)
+
+        self.full_tree.fit(data, labels)
+        separate1 = (data[:, 3] <= 0.374) & \
+                    (data[:, 16] > 0.003) & \
+                    (data[:, 74] > -0.002)
+        separate2 = (data[:, 3] > 0.374) & \
+                    (data[:, 98] <= 0.364) & \
+                    (data[:, 16] > 0.017)
+        partial_data = data[separate1 | separate2]
+        partial_labels = labels[separate1 | separate2]
+        self.partial_tree.fit(partial_data, partial_labels)
+
+    def classify(self, features):
+        features_mat = features.reshape((1, -1))
+        p_full = self.full_tree.predict(features_mat)[0]
+        p_partial = self.partial_tree.predict(features_mat)[0]
+
+        return np.round(self.full_weight * p_full + (1-self.full_weight)*p_partial)
+
+class contest_factory(abstract_classifier_factory):
+    def train(self, data, labels):
+        return contest_classifier(3, 0.7, data, labels)
